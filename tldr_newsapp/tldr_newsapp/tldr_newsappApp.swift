738b8845aeea573e70c384b55e2c60fe2fc35582
hvtran21 <huyytrann21@gmail.com>
2025-05-22 15:07:02 -0400

    Add default Swift template files. Learning basics of Swift and its syntax.

---
+++ tldr_newsapp/tldr_newsapp/tldr_newsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tldr_newsappApp.swift
+//  tldr_newsapp
+//
+//  Created by Huy Tran on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct tldr_newsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
