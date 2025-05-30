e6f3d35df77ee302a14a0d719c5113c976b31759
hvtran21 <huyytrann21@gmail.com>
2025-05-30 15:33:52 -0400

    Fixed styling issue for bottom and top nav bar

--- tldr_newsapp/app/components/styling.tsx
+++ tldr_newsapp/app/components/styling.tsx
@@ -4,49 +4,55 @@ import MaskedView from '@react-native-masked-view/masked-view';
 import { LinearGradient } from 'expo-linear-gradient';
 
 type GradientTextProps = {
-  text: string,
-  colors: [string, string], //  start color -> end color
-  style?: TextStyle,
-  summary?: string
-}
+    text: string;
+    colors: [string, string]; //  start color -> end color
+    style?: TextStyle;
+    summary?: string;
+};
 
 export const GradientText: React.FC<GradientTextProps> = ({ text, colors, style }) => {
-  return (
-    <MaskedView
-      maskElement={
-        <Text style={[style, { backgroundColor: 'transparent', color: 'black' }]}>
-          {text}
-        </Text>
-      }>
-      <LinearGradient
-        // gradient goes from left to right
-        colors={colors}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}>
-        <Text style={[style, { opacity: 0 }]}>{text}</Text>
-      </LinearGradient>
-    </MaskedView>
-  );
+    return (
+        <MaskedView
+            maskElement={
+                <Text style={[style, { backgroundColor: 'transparent', color: 'black' }]}>
+                    {text}
+                </Text>
+            }
+        >
+            <LinearGradient
+                // gradient goes from left to right
+                colors={colors}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+            >
+                <Text style={[style, { opacity: 0 }]}>{text}</Text>
+            </LinearGradient>
+        </MaskedView>
+    );
 };
 
 export const BaseTemplate = StyleSheet.create({
-  theme: {
-    flex: 1,
-    backgroundColor: '#000000'
-  },
+    theme: {
+        flex: 1,
+        backgroundColor: '#000000',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: '80%'
+    },
 
-  config: {
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+    config: {
+        flexDirection: 'column',
+        width: '100%',
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
-  title: {
-    fontFamily: 'Nunito-Bold',
-    fontSize: 22,
-  }
-})
+    title: {
+        fontFamily: 'Nunito-Bold',
+        fontSize: 30,
+    },
+});
 
-export default GradientText
+export default GradientText;
