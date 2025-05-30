ae30f2fa965a56dc0395c5334d71cfc5a09eaff9
hvtran21 <huyytrann21@gmail.com>
2025-05-30 15:34:20 -0400

    Applied prettier styling to format code for consistency

--- tldr_newsapp/app/components/news_card.tsx
+++ tldr_newsapp/app/components/news_card.tsx
@@ -1,148 +1,141 @@
 import { Text, View, StyleSheet, Image } from 'react-native';
 
-
 export type NewsCardProps = {
-  // define new type with required data members
-  title: string;
-  date: string;
-  genre: string;
-  image_src: any;
-}
+    // define new type with required data members
+    title: string;
+    date: string;
+    genre: string;
+    image_src: any;
+};
 
 export const NewsCard = ({ title, image_src, date, genre }: NewsCardProps) => {
-  // renders a news card based off of 'title' prop, and 'image_src' prop
-  return (
-    <View style={card_style.main_card}>
-      <View style={{ width: '60%', flexDirection: 'column', alignItems: 'flex-start'}}>
-        <Text style={card_style.date}>
-          {date} | {genre}
-        </Text>
-        <Text style={card_style.card_title}>
-          {title}
-        </Text>
-      </View>
+    // renders a news card based off of 'title' prop, and 'image_src' prop
+    return (
+        <View style={card_style.main_card}>
+            <View style={{ width: '60%', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Text style={card_style.date}>
+                    {date} | {genre}
+                </Text>
+                <Text style={card_style.card_title}>{title}</Text>
+            </View>
 
-      <View style={card_style.thumbnail_frame}>
-        <Image source={image_src} alt='Image' style={card_style.thumbnail_image} />
-      </View>
-    </View>
-  )
-}
+            <View style={card_style.thumbnail_frame}>
+                <Image source={image_src} alt="Image" style={card_style.thumbnail_image} />
+            </View>
+        </View>
+    );
+};
 
 export const HighlightCard = ({ title, image_src, date, genre }: NewsCardProps) => {
-  return(
-    <View style={card_style.highlight_card}>
-      <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'flex-start'}}>
-        
-        <Text style={card_style.date}>
-          {date} | {genre}
-        </Text> 
-        {/* TODO: Need to fix how highlight card is displayed.
+    return (
+        <View style={card_style.highlight_card}>
+            <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'flex-start' }}>
+                <Text style={card_style.date}>
+                    {date} | {genre}
+                </Text>
+                {/* TODO: Need to fix how highlight card is displayed.
                 -> currently leaving this for now, since The rest of the page needs to be
                       figured out.
         
         */}
-       
-        {/* <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-evenly' }}>
+
+                {/* <View style={{ width: '100%', flexDirection: 'column', justifyContent: 'space-evenly' }}>
           <View style={card_style.highlight_thumbnail}>
             <Image source={image_src} alt='Image' style={card_style.thumbnail_image} />
           </View> */}
-
-      </View>
-        {/* <View style={card_style.highlight_title}></View> */}
-
-    </View>
-  )
-}
+            </View>
+            {/* <View style={card_style.highlight_title}></View> */}
+        </View>
+    );
+};
 
 export const card_style = StyleSheet.create({
-  highlight_card: {
-    backgroundColor: '#141414',
-    padding: 5,
-    margin: 8,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    height: 300,
-    width: 'auto',
-  },
+    highlight_card: {
+        backgroundColor: '#141414',
+        padding: 5,
+        margin: 8,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        height: 300,
+        width: 'auto',
+    },
 
-  // TODO: Fix up styling for highlight card.
-  highlight_thumbnail: {
-    width: '100%',
-    height: '80%',
-    // padding: 5,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    // borderWidth: 1,
-    // borderColor: 'white',
-  },
+    // TODO: Fix up styling for highlight card.
+    highlight_thumbnail: {
+        width: '100%',
+        height: '80%',
+        // padding: 5,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        // borderWidth: 1,
+        // borderColor: 'white',
+    },
 
-  highlight_title_box: {
-    color: 'white',
-    // width: '100%',
-    // height: '35%',
-    backgroundColor: '#141414',
-    borderRadius: 5,
-  },
+    highlight_title_box: {
+        color: 'white',
+        // width: '100%',
+        // height: '35%',
+        backgroundColor: '#141414',
+        borderRadius: 5,
+    },
 
-  highlight_title_text: {
-    fontSize: 16,
-    fontFamily: 'Nunito-Light',
-  },
+    highlight_title_text: {
+        fontSize: 16,
+        fontFamily: 'Nunito-Light',
+    },
 
-  // styling for the news card itself
-  main_card: {
-    flexDirection: 'row',
-    backgroundColor: '#141414',
-    padding: 5,
-    margin: 8,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    maxHeight: 150,
-    width: 'auto'
-  },
+    // styling for the news card itself
+    main_card: {
+        flexDirection: 'row',
+        backgroundColor: '#141414',
+        padding: 5,
+        margin: 8,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        maxHeight: 150,
+        width: 'auto',
+    },
 
-  card_title: {
-    color: 'white',
-    fontSize: 16,
-    width: '100%',
-    height: '80%',
-    padding: 5,
-    fontFamily: 'Nunito-Light',
-    
-  },
+    card_title: {
+        color: 'white',
+        fontSize: 16,
+        width: '100%',
+        height: '80%',
+        padding: 5,
+        fontFamily: 'Nunito-Light',
+    },
 
-  thumbnail_frame: {
-    width: '40%',
-    padding: 5,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-  },
+    thumbnail_frame: {
+        width: '40%',
+        padding: 5,
+        borderRadius: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+    },
 
-  thumbnail_image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 5
-  },
+    thumbnail_image: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 5,
+    },
 
-  date: {
-    flexDirection: 'row',
-    fontSize: 11,
-    color: 'white',
-    fontFamily: 'Nunito-Light',
-    width: '100%',
-    padding: 5
-  }
+    date: {
+        flexDirection: 'row',
+        fontSize: 11,
+        color: 'white',
+        fontFamily: 'Nunito-Light',
+        width: '100%',
+        padding: 5,
+    },
 });
 
 export default NewsCard;
--- tldr_newsapp/app/index.tsx
+++ tldr_newsapp/app/index.tsx
@@ -1,36 +1,50 @@
-import * as React from 'react'
+import * as React from 'react';
 import { ScrollView, View, FlatList } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
-import { NewsCard, HighlightCard } from './components/news_card'
-import { BaseTemplate, GradientText } from './components/styling'
+import { NewsCard, HighlightCard } from './components/news_card';
+import { BaseTemplate, GradientText } from './components/styling';
+import { TopNavigation, BottomNavigation } from './components/navigation';
 
 export default function main() {
-  const [fontsLoaded] = useFonts ({
-  'Nunito-Light': require('../assets/fonts/Nunito/static/Nunito-Light.ttf'),
-  'Nunito-Bold': require('../assets/fonts/Nunito/static/Nunito-Bold.ttf')
-})
- const title='Hackers broke into Commvaults cloud backup system and stole secret passwords'
- const image_src=require('/Users/htran/repos/tldr_newsapp/tldr_newsapp/assets/images/computer.jpg')
- const date='May 24th'
- const genre='Cybersecurity'
+    const [fontsLoaded] = useFonts({
+        'Nunito-Light': require('../assets/fonts/Nunito/static/Nunito-Light.ttf'),
+        'Nunito-Medium': require('../assets/fonts/Nunito/static/Nunito-Medium.ttf'),
+        'Nunito-Bold': require('../assets/fonts/Nunito/static/Nunito-Bold.ttf'),
+    });
+    const title = 'Hackers broke into Commvaults cloud backup system and stole secret passwords';
+    const image_src = require('/Users/htran/repos/tldr_newsapp/tldr_newsapp/assets/images/computer.jpg');
+    const date = 'May 24th';
+    const genre = 'Cybersecurity';
 
- const title_2='Product-Led Growth (PLG) changed the game. Instead of being sold bloated software over steak dinners and locked into multi-year contracts'
- const image_src_2=require('/Users/htran/repos/tldr_newsapp/tldr_newsapp/assets/images/computer_2.jpg')
- const date_2='May 24th'
- const genre_2='AI'
+    const title_2 =
+        'Product-Led Growth (PLG) changed the game. Instead of being sold bloated software over steak dinners and locked into multi-year contracts';
+    const image_src_2 = require('/Users/htran/repos/tldr_newsapp/tldr_newsapp/assets/images/computer_2.jpg');
+    const date_2 = 'May 24th';
+    const genre_2 = 'AI';
 
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={BaseTemplate.theme} edges={['top', 'left', 'right', 'bottom']}>
-        <View style={BaseTemplate.config}>
-          <GradientText colors={["#C54B8C", "#6A5ACD"]} text='TLDR Newsletter' style={BaseTemplate.title}></GradientText>
-          <ScrollView>
-            <HighlightCard title={title_2} image_src={image_src_2} date={date_2} genre={genre_2} />
-            <NewsCard title={title} image_src={image_src} date={date} genre={genre} />
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={BaseTemplate.theme} edges={['top', 'left', 'right', 'bottom']}>
+                <View style={BaseTemplate.config}>
+                    <GradientText
+                        colors={['#C54B8C', '#6A5ACD']}
+                        text="TLDR Newsletter"
+                        style={BaseTemplate.title}
+                    ></GradientText>
+                    <TopNavigation />
+                    <ScrollView>
+                        <HighlightCard
+                            title={title_2}
+                            image_src={image_src_2}
+                            date={date_2}
+                            genre={genre_2}
+                        />
+                        <NewsCard title={title} image_src={image_src} date={date} genre={genre} />
+                    </ScrollView>
+                </View>
+                <BottomNavigation />
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
