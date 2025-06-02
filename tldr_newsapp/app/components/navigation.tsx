cd3efc73839cefb5f0da8a5c466d74e66c9e5c83
hvtran21 <huyytrann21@gmail.com>
2025-06-01 21:44:46 -0400

    Finished up the UI of the bottom navigation bar

--- tldr_newsapp/app/components/navigation.tsx
+++ tldr_newsapp/app/components/navigation.tsx
@@ -1,10 +1,13 @@
-import { View, StyleSheet, ScrollView, Text } from 'react-native';
-import React from 'react';
+import { View, StyleSheet, ScrollView, Text, ImageURISource, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHouse, faUser, faMagnifyingGlass, faBookmark } from '@fortawesome/free-solid-svg-icons'
+import {  } from '@fortawesome/free-regular-svg-icons'
 
 export const TopNavigation = () => {
     return (
         <View style={TopNavBar.nav_bar_container}>
-            <ScrollView horizontal={true} contentContainerStyle={TopNavBar.scroll_content}>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} contentContainerStyle={TopNavBar.scroll_content}>
                 <View style={TopNavBar.single_nav}>
                     <Text style={TopNavBar.single_nav_text}>Top News</Text>
                 </View>
@@ -17,15 +20,23 @@ export const TopNavigation = () => {
                 <View style={TopNavBar.single_nav}>
                     <Text style={TopNavBar.single_nav_text}>DevOps</Text>
                 </View>
+                <View style={TopNavBar.single_nav}>
+                    <Text style={TopNavBar.single_nav_text}>AI</Text>
+                </View>
             </ScrollView>
         </View>
     );
 };
 
-export const BottomNavigation = () => {
+export function BottomNavigation() {
     return (
         <View style={BottomNavBar.nav_bar_container}>
-
+            <View style={BottomNavBar.nav_icon_config}>
+                <FontAwesomeIcon icon={faUser} style={{ color: 'white' }}/>
+                <FontAwesomeIcon icon={faHouse} style={{ color: 'white' }}/>
+                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: 'white' }}/>
+                <FontAwesomeIcon icon={faBookmark} style={{ color: 'white' }}/>
+            </View>
         </View>
     )
 };
@@ -33,37 +44,41 @@ export const BottomNavigation = () => {
 const BottomNavBar = StyleSheet.create({
     nav_bar_container: {
         width: '100%',
-        height: '10%',
-        margin: 8,
-        justifyContent: 'center',
+        height: '5%',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        borderRadius: 5,
-        backgroundColor: '#141414',
-
+        backgroundColor: '#000000',
     },
+
+    nav_icon_config: {
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '100%',
+        paddingTop: 15,
+
+    }
 });
 
 const TopNavBar = StyleSheet.create({
     nav_bar_container: {
-        width: '100%',
         height: '10%',
-        margin: 8,
-        justifyContent: 'center',
-        alignItems: 'center',
+        borderRadius: 20
     },
 
     scroll_content: {
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         flexDirection: 'row',
         alignItems: 'center',
-        minWidth: '100%',
     },
 
     single_nav: {
-        width: 90,
+        width: 85,
         height: 35,
         borderRadius: 15,
-        margin: 5,
+        margin: 6,
+        padding: 2,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#141414',
