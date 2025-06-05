1e95751d38fd6702155e1514839da2a9c741c11b
hvtran21 <huyytrann21@gmail.com>
2025-06-05 16:08:11 -0400

    Update and fix start server script

--- src/start-server.sh
+++ src/start-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/server"
+rm -rf ./dist/*
 npx tsc
-npx concurrently "npx tsc -w" "nodemon dist/index.js"
+npx concurrently "npx tsc -w" "nodemon dist/src/index.js"
