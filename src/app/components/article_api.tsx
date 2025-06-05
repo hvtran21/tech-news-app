6f8be0cdd856eb60d6b51a471f00136a61d1c1da
hvtran21 <huyytrann21@gmail.com>
2025-06-04 20:41:13 -0400

    Add article api tsx file to fetch deta

---
+++ src/app/components/article_api.tsx
@@ -0,0 +1,7 @@
+import { NewsCardProps } from './news_card'
+ 
+export const getArticle = ({title, image_src, date, genre }: NewsCardProps) => {
+
+    const BASE_URL = `https://newsapi.org/v2/`
+    
+}
