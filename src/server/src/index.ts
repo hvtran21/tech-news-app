4d53d3e6b0a4b29478f9bb94cc20fca9664c13a4
hvtran21 <huyytrann21@gmail.com>
2025-06-05 16:27:26 -0400

    Add database connection and initialization

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,16 +1,30 @@
 import express, { Express, Request, Response } from 'express';
+import db from '../db'
+import { articleTableDefinition } from '../models';
 
-const app  = express();
-const port = 8000;
+async function initDatabase() {
+    try {
+        await db.none(articleTableDefinition);
+        console.log('Database initialized...');
+    } catch (error) {
+        console.error('Error ocurred when initializing Database: ', error);
+        return;
+    }
+};
 
-app.get('/', (req, res) => {
-    res.send("HELLO!");
-});
 
-app.get('/different', (req, res) => {
-    res.send("HELLO! but this is different?");
-});
+const serverStart = () => {
+    const server  = express();
+    const port = 8000;
+    
+    server.get('/', (req, res) => {
+        res.send("HELLO!");
+    });
+    
+    server.listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    });
+};
 
-app.listen(port, () => {
-    console.log(`now listening on port ${port}`);
-});
+initDatabase();
+serverStart();
