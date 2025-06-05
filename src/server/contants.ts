71c07b72ad2fa7abb9cd103fe44f01f774f55f5f
hvtran21 <huyytrann21@gmail.com>
2025-06-05 16:27:54 -0400

    Add constants.ts, added new enum type

---
+++ src/server/contants.ts
@@ -0,0 +1,9 @@
+export enum techGenres {
+    AI = 'ai',
+    DEVOPS = 'devops',
+    WEBDEV = 'webdev',
+    IT = 'it',
+    ROBOTICS = 'robotics',
+    TOP_NEWS_TECH = 'top news tech',
+    ENGINEERING = 'engineering'
+};
