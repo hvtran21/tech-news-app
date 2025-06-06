3fc4f0b27bac3f2038604da8638f2c8657466fd4
hvtran21 <huyytrann21@gmail.com>
2025-06-06 18:20:45 -0400

    Update the article interface

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -1,14 +1,14 @@
 // this is what's used when connecting to the backend API endpoint
-export interface article {
+interface article {
     id: string;
     source: string;
-    author: string
+    author: string;
     title: string;
     description: string;
     url: URL;
     urlToImage: URL;
     publishedAt: string;
-    content: string | undefined;
+    content?: string;
 }
 
 export default article;
