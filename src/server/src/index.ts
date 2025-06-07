bb8cebf6c6626798cfe06f273e61c90dc2975d31
hvtran21 <huyytrann21@gmail.com>
2025-06-07 10:38:08 -0400

    Add api endpoints for frontend to request. Added a function that retrieves articles from all genres and stores them into datbase

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,7 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import { articleTableDefinition } from '../models';
 import fetchArticles from '../newsapi';
+import techGenres from '../constants';
 import db from '../db'
+
+// TODO: add a way so data is continuously retrieved. Similar to a scheduler
 
 async function initDatabase() {
     try {
@@ -13,12 +16,40 @@ async function initDatabase() {
     }
 };
 
+async function retrieveData() {
+    for (const val of Object.values(techGenres)) {
+        fetchArticles(val);
+    }
+};
+
 const serverStart = () => {
     const server  = express();
+    server.use(express.json());
     const port = process.env.PORT;
 
-    server.get('/api', (req, res) => {
-        fetchArticles();
+    server.get('/fetch', (req, res) => {
+        retrieveData();
+    });
+
+    server.get('/api/articles', (req, res) => {
+        // parse POST request
+        const genre = req.body.genre;
+
+        // send JSON response back
+        try {
+            // fetch data from db
+            db.any('SELECT * FROM articles WEHRE genre = $1', [genre])
+                // data fetched, send response back as JSON
+                .then(data => {
+                    res.json(data);
+                })
+                .catch(error => {
+                    console.error(`Error ocurred when retrieving article data: ${error}`);
+                });
+
+        } catch (error) {
+            console.error(`Error occurred: ${error}`);
+        };      
     })
 
     server.listen(port, () => {
