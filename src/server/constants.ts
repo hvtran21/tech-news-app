199be96020469664ee67eb4908aca311a634a6f6
hvtran21 <huyytrann21@gmail.com>
2025-06-07 10:38:22 -0400

    Update constants

---
+++ src/server/constants.ts
@@ -0,0 +1,15 @@
+enum techGenres {
+    AI = 'artificial intelligence',
+    DEVOPS = 'development operations',
+    WEBDEV = 'web development',
+    IT = 'information technology',
+    ROBOTICS = 'robotics',
+    TOP_NEWS_TECH = 'top tech news',
+    ENGINEERING = 'engineering',
+    START_UPS = 'Top start ups',
+    GOOGLE = 'Google',
+    APPLE = 'Apple',
+
+};
+
+export default techGenres;
