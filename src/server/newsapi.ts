c82b71a140779edf832a6a92a197b9c86c6d5573
hvtran21 <huyytrann21@gmail.com>
2025-06-07 10:38:44 -0400

    Modified fetchArticles to take in a genre parameter

--- src/server/newsapi.ts
+++ src/server/newsapi.ts
@@ -1,4 +1,3 @@
-import { techGenres } from "./contants";
 import db from "./db";
 import dotenv from 'dotenv';
 import {v4 as uuidv4} from 'uuid';
@@ -19,15 +18,12 @@ interface Article {
     content?: string;
 }
 
-async function fetchArticles() {
+async function fetchArticles(genre: string) {
     // Parameters: None
     // Return: None
     // Fetches a news article from News API, and adds it to database
 
-    // TODO: fetch articles based off of user preference, this will be added at a later point.
-    const genre = techGenres.AI;
     const apiKey = process.env.NEWS_API_KEY;
-    const user_country = 'us'
     var pageSize = 20;
     var base_url = `https://newsapi.org/v2/top-headlines?q=${genre}&apiKey=${apiKey}&pageSize=${pageSize}`;
 
@@ -37,15 +33,15 @@ async function fetchArticles() {
     while (totalProcesssed < totalResults) {
         // fetch data from News API
         try {
+            // if there are more results, need to fetch all of them.
             var url = base_url;
             if (totalResults > pageSize) {
                 console.log(`Fetching page=${page}`);
-                // if there are more results, need to fetch all of them.
                 url = `${base_url}&page=${page}`;
                 page += 1;
             }
             const encoded_url = encodeURI(url);
-            console.log('fetching article..')
+            console.log('fetching data...')
             const response = await fetch(encoded_url);
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
@@ -81,8 +77,8 @@ async function fetchArticles() {
         db.tx(t => {
             const queries = lst.map(article => {
                 return t.none(
-                    'INSERT INTO articles(id, source, author, title, description, url, url_to_image, published_at, content) \
-                        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)', [uuidv4(), article.source, article.author, article.title, article.description, article.url, article.url_to_image, article.published_at, article.content]
+                    'INSERT INTO articles(id, genre, source, author, title, description, url, url_to_image, published_at, content) \
+                        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [uuidv4(), genre, article.source, article.author, article.title, article.description, article.url, article.url_to_image, article.published_at, article.content]
                 );
             });
             return t.batch(queries);
