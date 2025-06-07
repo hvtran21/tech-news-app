82f925e8464df617a27c87a67b806fe8a3ee2f54
hvtran21 <huyytrann21@gmail.com>
2025-06-07 10:40:33 -0400

    Modify client API endpoint to request for articles based off of genre

--- src/app/components/article_api.tsx
+++ src/app/components/article_api.tsx
@@ -1,7 +1,27 @@
 import { Config } from "react-native-config"
+import { techGenres } from "./article";
 
-const retrieveArticles = () => {
+async function retrieveArticles() {
+    try {
+        var response = await fetch(`${Config.BASE_URL}/api/articles`, {
+            // TODO: Add authorization somewhere. Maybe here, maybe not.
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                genre: techGenres.AI,
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(`Error ocurred, response status: ${response.status}`);
+        }
+        var data = await response.json();
 
+    } catch (error) {
+        console.error(`Error ocurred: ${error}`);
+    }
 }
 
 export default retrieveArticles;
