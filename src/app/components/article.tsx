57e479852a5128905c6f22362f8ebfaa8df472b1
hvtran21 <huyytrann21@gmail.com>
2025-06-07 10:39:37 -0400

    Add enum techGenres

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -11,4 +11,18 @@ interface article {
     content?: string;
 }
 
+enum techGenres {
+    AI = 'artificial intelligence',
+    DEVOPS = 'development operations',
+    WEBDEV = 'web development',
+    IT = 'information technology',
+    ROBOTICS = 'robotics',
+    TOP_NEWS_TECH = 'top tech news',
+    ENGINEERING = 'engineering',
+    START_UPS = 'Top start ups',
+    GOOGLE = 'Google',
+    APPLE = 'Apple',
+}
+
 export default article;
+export {techGenres};
