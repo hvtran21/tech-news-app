725d58100683f9c132a148e0d9b5dc87a271499b
hvtran21 <huyytrann21@gmail.com>
2025-06-07 22:08:58 -0400

    Update new enum types

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,15 +1,19 @@
 enum techGenres {
-    AI = 'artificial intelligence',
-    DEVOPS = 'development operations',
-    WEBDEV = 'web development',
-    IT = 'information technology',
-    ROBOTICS = 'robotics',
-    TOP_NEWS_TECH = 'top tech news',
-    ENGINEERING = 'engineering',
-    START_UPS = 'Top start ups',
+    AI = 'Artificial Intelligence',
+    DEVOPS = 'Development Operations',
+    WEBDEV = 'Web Development',
+    INFO_TECH = 'Information Iechnology',
+    ROBOTICS = 'Robotics',
+    BIG_TECH = 'Big Tech',
+    // ENGINEERING = 'Engineering',
+    START_UPS = 'Start-ups',
     GOOGLE = 'Google',
     APPLE = 'Apple',
-
+    MICROSOFT = 'Microsoft',
+    AMAZON = 'Amazon',
+    GAMING = 'Gaming',
+    CYBERSECURITY = 'Cybersecurity',
+    GAME_DEV = 'Grame development'
 };
 
 export default techGenres;
