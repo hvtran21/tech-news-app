8c75483f5b6714f59a9ac5142f2942bfd9fab665
hvtran21 <huyytrann21@gmail.com>
2025-06-07 22:08:10 -0400

    small modification

--- src/app/db/db.tsx
+++ src/app/db/db.tsx
@@ -1,6 +1,8 @@
 import * as SQLite from 'expo-sqlite';
 import retrieveArticles from '../components/article_api';
 
-const db = SQLite.openDatabaseAsync('articles.db');
+const db = SQLite.openDatabaseAsync('articles.db');;
 
-
+export function addArticles() {
+    
+}
