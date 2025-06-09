f62b9b773a2af592fa319d16fb06d750e137e0f9
hvtran21 <huyytrann21@gmail.com>
2025-06-08 20:40:31 -0400

    Testing backend API endpoint for data obtainment

--- src/app/db/db.tsx
+++ src/app/db/db.tsx
@@ -1,8 +1,33 @@
 import * as SQLite from 'expo-sqlite';
-import retrieveArticles from '../components/article_api';
+import { Config } from "react-native-config"
+import { techGenres } from '../components/article';
 
-const db = SQLite.openDatabaseAsync('articles.db');;
+const db = SQLite.openDatabaseAsync('articles.db');
+const BASE_URL = 'http://localhost:8000'
 
-export function addArticles() {
-    
+async function retrieveArticles(genres: string) {
+    try {
+        var response = await fetch(`${BASE_URL}/api/articles`, {
+            // TODO: Add authorization somewhere. Maybe here, maybe not.
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                genre: {genres}, // e.g., 'Artificial Intelligence,Dev Ops,Web Dev'
+            }),
+        });
+        if (!response.ok) {
+            throw new Error(`Error ocurred, response status: ${response.status}`);
+        }
+        var data = await response.json();
+        console.log('data:', data)
+
+
+    } catch (error) {
+        console.error(`Error ocurred: ${error}`);
+    }
 }
+
+export default retrieveArticles;
