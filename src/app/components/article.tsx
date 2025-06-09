75b1736a4a73c199dd81db15b9836e7ad664c032
hvtran21 <huyytrann21@gmail.com>
2025-06-09 15:39:32 -0400

    Update Enum type. Honestly, these definitions need to be moved

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -13,20 +13,17 @@ interface article {
 
 enum techGenres {
     AI = 'Artificial Intelligence',
-    DEVOPS = 'Dev Ops',
-    WEBDEV = 'Web Dev',
+    ML = 'Machine Learning',
     INFO_TECH = 'Information Iechnology',
     ROBOTICS = 'Robotics',
     BIG_TECH = 'Big Tech',
-    // ENGINEERING = 'Engineering',
-    START_UPS = 'Start-ups',
-    GOOGLE = 'Google',
+    START_UPS = 'Start ups',
     APPLE = 'Apple',
     MICROSOFT = 'Microsoft',
     AMAZON = 'Amazon',
     GAMING = 'Gaming',
     CYBERSECURITY = 'Cybersecurity',
-    GAME_DEV = 'Game development'
+    GAME_DEVELOPMENT = 'Game development'
 }
 
 export default article;
