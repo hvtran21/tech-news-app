dc69e6a44c149d931db24993a06f9adb42dc0d56
hvtran21 <huyytrann21@gmail.com>
2025-06-10 10:51:13 -0400

    Add back the values to the ENUM

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,16 +1,16 @@
 enum techGenres {
-    AI,
-    ML,
-    INFO_TECH,
-    ROBOTICS,
-    BIG_TECH,
-    START_UPS,
-    APPLE,
-    MICROSOFT,
-    AMAZON,
-    GAMING,
-    CYBERSECURITY,
-    GAME_DEVELOPMENT,
+    AI = 'Artificial Intelligence',
+    ML = 'Machine Learning',
+    APPLE = 'Apple',
+    INFO_TECH = 'Information Iechnology',
+    ROBOTICS = 'Robotics',
+    BIG_TECH = 'Big Tech',
+    START_UPS = 'Start ups',
+    MICROSOFT = 'Microsoft',
+    AMAZON = 'Amazon',
+    GAMING = 'Gaming',
+    CYBERSECURITY = 'Cybersecurity',
+    GAME_DEVELOPMENT = 'Game development'
 };
 
 export default techGenres;
