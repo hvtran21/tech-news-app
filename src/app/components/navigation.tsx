be01c5dd4366c570f9c63123247eda2168bcb894
hvtran21 <huyytrann21@gmail.com>
2025-06-10 20:57:04 -0400

    Remove top nav bar, replacing it with a cleaner, familiar look

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,39 +1,7 @@
-import { View, StyleSheet, ScrollView, Text, ImageURISource, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUser, faMagnifyingGlass, faBookmark } from '@fortawesome/free-solid-svg-icons';
-import { genre } from '../homepage';
-
-export const TopNavigation = ({value}: genre) => {
-    const [genres, setGenres] = useState<string[]>([]);
-    useEffect(() => {
-        const get_genres = () => {
-            setGenres(value.split(','));
-        }
-        get_genres();
-    }, [value])
-    
-    return (
-        <View style={TopNavBar.nav_bar_container}>
-            <ScrollView
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={TopNavBar.scroll_content}
-            >
-                <TouchableOpacity style={TopNavBar.single_nav}>
-                    <Text style={TopNavBar.single_nav_text}>Top News</Text>
-                </TouchableOpacity>
-                {genres.map((genre, index) => {
-                    return (
-                        <TouchableOpacity key={index} style={TopNavBar.single_nav}>
-                            <Text style={TopNavBar.single_nav_text}>{genre}</Text>
-                        </TouchableOpacity>
-                    )
-                })}
-            </ScrollView>
-        </View>
-    );
-};
 
 export function BottomNavigation() {
     return (
@@ -68,37 +36,4 @@ const BottomNavBar = StyleSheet.create({
     },
 });
 
-const TopNavBar = StyleSheet.create({
-    nav_bar_container: {
-        height: '7%',
-        marginTop: 5,
-        borderBottomColor: '#141414',
-        borderWidth: StyleSheet.hairlineWidth
-    },
-
-    scroll_content: {
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-
-    single_nav: {
-        width: 85,
-        height: 35,
-        borderRadius: 15,
-        margin: 6,
-        padding: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#141414',
-    },
-
-    single_nav_text: {
-        fontSize: 14,
-        fontFamily: 'WorkSans-Light',
-        color: 'white',
-        opacity: 0.8
-    },
-});
-
-export default TopNavigation;
+export default BottomNavBar;
