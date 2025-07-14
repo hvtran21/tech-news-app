7c8ff87482ac90f78f3e49367880b8e713f04a6b
hvtran21 <huyytrann21@gmail.com>
2025-07-14 18:40:09 -0400

    Add card flip to show backside, skeleton for backside too. Applied prettier formatting

--- src/app/components/news_card.tsx
+++ src/app/components/news_card.tsx
@@ -1,15 +1,24 @@
-import { Text, View, StyleSheet, Image, Pressable, TouchableOpacity, TouchableHighlight } from 'react-native';
+import {
+    Text,
+    View,
+    StyleSheet,
+    Image,
+    Dimensions,
+    TouchableOpacity,
+    TouchableHighlight,
+    ViewStyle,
+} from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEllipsisV, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { card } from '../homepage';
 import Animated, {
     interpolate,
     useAnimatedStyle,
     withTiming,
+    SharedValue,
+    useSharedValue,
 } from 'react-native-reanimated';
-import { url } from 'inspector';
-import { setDefaultAutoSelectFamilyAttemptTimeout } from 'net';
 
 function formatDate(date: Date): string {
     if (!(date instanceof Date) || isNaN(date.getTime())) {
@@ -53,81 +62,36 @@ function formatDate(date: Date): string {
     return `${month} ${day}${getOrdinalSuffix(day)}`;
 }
 
-// type FlipCardProps = {
-//     isFlipped: boolean
-//     cardStyle: React.CSSProperties
-//     direction: string
-//     duration: number
-//     RegularContent: React.ReactNode
-//     FlippedContent: React.ReactNode
-// }
-
-// const FlipCard = ({
-//   isFlipped,
-//   cardStyle,
-//   direction = 'y',
-//   duration = 500,
-//   RegularContent,
-//   FlippedContent,
-// }: FlipCardProps) => {
-//   const isDirectionX = direction === 'x';
-
-//   const regularCardAnimatedStyle = useAnimatedStyle(() => {
-//     const spinValue = interpolate(Number(isFlipped.value), [0, 1], [0, 180]);
-//     const rotateValue = withTiming(`${spinValue}deg`, { duration });
-
-//     return {
-//       transform: [
-//         isDirectionX ? { rotateX: rotateValue } : { rotateY: rotateValue },
-//       ],
-//     };
-//   });
-
-//   const flippedCardAnimatedStyle = useAnimatedStyle(() => {
-//     const spinValue = interpolate(Number(isFlipped.value), [0, 1], [180, 360]);
-//     const rotateValue = withTiming(`${spinValue}deg`, { duration });
-
-//     return {
-//       transform: [
-//         isDirectionX ? { rotateX: rotateValue } : { rotateY: rotateValue },
-//       ],
-//     };
-//   });
-
-//   return (
-//     <View>
-//       <Animated.View
-//         style={[
-//           flipCardStyles.regularCard,
-//           cardStyle,
-//           regularCardAnimatedStyle,
-//         ]}>
-//         {RegularContent}
-//       </Animated.View>
-//       <Animated.View
-//         style={[
-//           flipCardStyles.flippedCard,
-//           cardStyle,
-//           flippedCardAnimatedStyle,
-//         ]}>
-//         {FlippedContent}
-//       </Animated.View>
-//     </View>
-//   );
-
-export const NewsCardFront = ({ title, url_to_image, published_at, genre, id, handleEllipsisPress}: card) => {
+export const NewsCardFront = ({
+    title,
+    url_to_image,
+    published_at,
+    genre,
+    id,
+    handleEllipsisPress,
+}: card) => {
     const [imageLoaded, setImageLoaded] = useState(false);
     const [imageError, setImageError] = useState(false);
     const [badLoad, setBadLoad] = useState(false);
     const date = formatDate(new Date(published_at));
     const fallBackImage = require('../../assets/images/computer_2.jpg');
-    const uri_image = url_to_image ? { uri: url_to_image } : {uri: fallBackImage};
+    const uri_image = url_to_image ? { uri: url_to_image } : { uri: fallBackImage };
     const label = genre === '' ? 'Top' : genre;
 
     return (
-                <View style={card_style.main_card}>
-            <TouchableOpacity style={{ position: 'absolute', top: 0, right: 14 }} onPress={() => {handleEllipsisPress(id)}}>
-                <FontAwesomeIcon icon={faEllipsisH} color='white' size={18} style={{ opacity: 0.5, marginBottom: 10 }}/>
+        <View style={card_style_front.main_card}>
+            <TouchableOpacity
+                style={{ position: 'absolute', top: 0, right: 14 }}
+                onPress={() => {
+                    handleEllipsisPress(id);
+                }}
+            >
+                <FontAwesomeIcon
+                    icon={faEllipsisH}
+                    color="white"
+                    size={18}
+                    style={{ opacity: 0.5, marginBottom: 10 }}
+                />
             </TouchableOpacity>
             <View
                 style={{
@@ -136,76 +100,169 @@ export const NewsCardFront = ({ title, url_to_image, published_at, genre, id, ha
                 }}
             >
                 <View style={{ width: '60%', flexDirection: 'column', height: 'auto' }}>
-                    <Text style={card_style.date}>
+                    <Text style={card_style_front.date}>
                         {date} | {label}
                     </Text>
                     <View style={{ width: '95%' }}>
-                        <Text style={card_style.card_title}>{title}</Text>
+                        <Text style={card_style_front.card_title}>{title}</Text>
                     </View>
                 </View>
 
-                <View style={card_style.thumbnail_frame}>
+                <View style={card_style_front.thumbnail_frame}>
                     {badLoad && !imageLoaded && (
-                        <Image 
-                        source={fallBackImage}
-                        alt='Image'
-                        style={card_style.thumbnail_image}
+                        <Image
+                            source={fallBackImage}
+                            alt="Image"
+                            style={card_style_front.thumbnail_image}
                         />
                     )}
                     {!imageError && (
                         <Image
                             source={imageError ? fallBackImage : uri_image}
-                            alt='Image'
-                            style={card_style.thumbnail_image}
+                            alt="Image"
+                            style={card_style_front.thumbnail_image}
                             onLoad={(e) => {}}
-                            onError={() => {setImageError(true)}}
+                            onError={() => {
+                                setImageError(true);
+                            }}
                         />
                     )}
                 </View>
             </View>
         </View>
-    )
-}
+    );
+};
 
-export const NewsCardBack = () => {
-    
-}
+type FlipCardProps = {
+    isFlipped: SharedValue<boolean>;
+    cardStyle: ViewStyle;
+    direction: string;
+    duration: number;
+    RegularContent: React.ReactNode;
+    FlippedContent: React.ReactNode;
+};
 
-export const NewsCard = ({ title, url_to_image, published_at, genre, id, handleEllipsisPress }: card) => {
-    const [flipped, setFlipped] = useState(false);
-    
-    useEffect(() => {
-        if (!flipped) {
-            console.log('Card facing front')
-        } else {
-            console.log('Card facing back')
-        }
+const FlipCard = ({
+    isFlipped,
+    cardStyle,
+    direction = 'y',
+    duration = 500,
+    RegularContent,
+    FlippedContent,
+}: FlipCardProps) => {
+    const isDirectionX = direction === 'x';
 
-    }, [flipped])
+    const regularCardAnimatedStyle = useAnimatedStyle(() => {
+        const spinValue = interpolate(Number(isFlipped.value), [0, 1], [0, 180]);
+        const rotateValue = withTiming(`${spinValue}deg`, { duration });
+
+        return {
+            transform: [isDirectionX ? { rotateX: rotateValue } : { rotateY: rotateValue }],
+        };
+    });
+
+    const flippedCardAnimatedStyle = useAnimatedStyle(() => {
+        const spinValue = interpolate(Number(isFlipped.value), [0, 1], [180, 360]);
+        const rotateValue = withTiming(`${spinValue}deg`, { duration });
+
+        return {
+            transform: [isDirectionX ? { rotateX: rotateValue } : { rotateY: rotateValue }],
+        };
+    });
 
     return (
-        <TouchableHighlight onPress={() => {setFlipped((state) => !state)}}>
-            <View>
-                <NewsCardFront
-                    title={title}
-                    url_to_image={url_to_image}
-                    published_at={published_at}
-                    genre={genre}
-                    id={id}
-                    handleEllipsisPress={handleEllipsisPress}
-                />
-            </View>
+        <View>
+            <Animated.View
+                style={[flipCardStyles.regularCard, cardStyle, regularCardAnimatedStyle]}
+            >
+                {RegularContent}
+            </Animated.View>
+            <Animated.View
+                style={[flipCardStyles.flippedCard, cardStyle, flippedCardAnimatedStyle]}
+            >
+                {FlippedContent}
+            </Animated.View>
+        </View>
+    );
+};
+
+type cardBackProps = {
+    id: string;
+};
+
+export const NewsCardBack = ({ id }: cardBackProps) => {
+    return (
+        <View style={card_style_back.main_card}>
+            <Text style={{ color: 'white' }}>This is the back</Text>
+        </View>
+    );
+};
+
+export const NewsCard = ({
+    title,
+    url_to_image,
+    published_at,
+    genre,
+    id,
+    handleEllipsisPress,
+}: card) => {
+    const isFlipped = useSharedValue(false);
+    const handlePress = () => {
+        isFlipped.value = !isFlipped.value;
+    };
+    const screenWidth = Dimensions.get('window').width;
+
+    return (
+        <TouchableHighlight onPress={handlePress} style={{ width: screenWidth }}>
+            <FlipCard
+                isFlipped={isFlipped}
+                cardStyle={flipCardStyles.flipCard}
+                direction="x"
+                duration={500}
+                FlippedContent={<NewsCardBack id={id} />}
+                RegularContent={
+                    <NewsCardFront
+                        title={title}
+                        url_to_image={url_to_image}
+                        published_at={published_at}
+                        genre={genre}
+                        id={id}
+                        handleEllipsisPress={handleEllipsisPress}
+                    />
+                }
+            />
         </TouchableHighlight>
     );
 };
 
-export const card_style = StyleSheet.create({
-    // styling for the news card itself
+const flipCardStyles = StyleSheet.create({
+    flippedCard: {
+        zIndex: 2,
+    },
+
+    regularCard: {
+        position: 'absolute',
+        zIndex: 1,
+    },
+
+    flipCard: {
+        backfaceVisibility: 'hidden',
+    },
+});
+
+const card_style_back = StyleSheet.create({
+    main_card: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: 150,
+    },
+});
+
+export const card_style_front = StyleSheet.create({
     main_card: {
         flexDirection: 'row',
         backgroundColor: '#000000',
         maxHeight: 175,
-        width: '98%',
         alignContent: 'center',
         justifyContent: 'center',
         height: 150,
