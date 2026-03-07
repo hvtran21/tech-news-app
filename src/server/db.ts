d5fa6694628dd6cc8edd898125797476f5375986
hvtran21 <huyytrann21@gmail.com>
2026-03-07 00:15:12 -0500

    use env vars

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,14 @@
 import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const cn = {
-    host: 'localhost',
-    port: 5432,
-    database: 'newsapp',
-    user: 'dev',
-    password: 'dev',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    database: process.env.DB_NAME || 'newsapp',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 };
 
 const pgp = pgPromise();
